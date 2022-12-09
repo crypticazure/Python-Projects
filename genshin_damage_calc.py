9dc84d499647514561ebb56fc329472bbb0fe00f
Dylan <109814714+dylancortez727@users.noreply.github.com>
2022-12-08 22:17:39 -0600

    init commit

---
+++ genshin_damage_calc.py
@@ -0,0 +1,4 @@
+"""Genshin Impact is an action RPG available for PlayStation, PC, and Mobile devices. It has an in-depth combat system that requires multiple calculations for a variety of stats.
+This program seeks to take a character and their stats, and be able to plan out damage numbers for them depending on the multiple factors that come into play with Genshin Impact."""
+
+#First, we will describe the damage calculation
